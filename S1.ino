16f8e20cfb8d6f8d9c6ecd41025cbff87bc8a29d
Eduardo_ducci <eduardo_ducci@estudante.sesisenai.org.br>
2025-11-30 22:45:20 -0300

    Commit-S1

--- S1.ino
+++ S1.ino
@@ -1,64 +1,109 @@
+
+#include <WiFi.h>
 #include <WiFiClientSecure.h>
- #include<PubSubClient.h>
+#include <PubSubClient.h>
 
 WiFiClientSecure client;
 PubSubClient mqtt(client);
 
- const String SSID = "FIESC_IOT_EDU";
- const String PASS = "8120gv08";
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
-const String URL = "2941e5c6678a4be18375b50465ad0964.s1.eu.hivemq.cloud";
-const int PORT = 8883;
-const String USR = "admin";
-const String Broker_PASS= "SAjoao10";
-const String MyTopic = "";
-const String OtherTopic = "";
+const String URL   = "2941e5c6678a4be18375b50465ad0964.s1.eu.hivemq.cloud";
+const int PORT     = 8883;
+const String broker_USR   = "Placa-S1-Eduardo";
+const String broker_PASS  = "SAEduardo1";
 
+const String MyTopic = "sensor/comando";
+const String OtherTopic = "sensor/distancia";
 
+const int trigPin = 22;
+const int echoPin = 23;
+const int ledPin  = 19;
 
+long duracao;
+float distancia;
+bool detectou = false;
 
- void setup() {
+void setup() {
   Serial.begin(115200);
-  Serial.println("Conectando ao WiFI");
-  WiFi.begin(SSID,PASS);
-  while(WiFi.status() != WL_CONNECTED){
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Serial.println("Conectando ao WiFi");
+  WiFi.begin(SSID.c_str(), PASS.c_str());
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
+
   Serial.println("\nConectado com sucesso!");
   client.setInsecure();
   Serial.println("Conectando ao Broker");
-  mqtt.setServer(URL.c_str(),PORT);
-  while(!mqtt.connected()){
-    String ID = "S2_";
-    ID += String(random(0xffff),HEX);
-    mqtt.connect(ID.c_str(), USR.c_str(), Broker_PASS.c_str());
+
+  mqtt.setServer(URL.c_str(), PORT);
+  mqtt.setCallback(callback);
+
+  while (!mqtt.connected()) {
+    String ID = "s1_";
+    ID += String(random(0xffff), HEX);
+    mqtt.connect(ID.c_str(), broker_USR.c_str(), broker_PASS.c_str());
     Serial.print(".");
     delay(200);
   }
+
   mqtt.subscribe(MyTopic.c_str());
-  mqtt.setCallback(callback);
   Serial.println("\nConectado com sucesso ao broker!");
-
+  digitalWrite(ledPin, LOW);
 }
-
 
 void loop() {
-  String mensagem = "Jaison: ";
-  if (Serial.available()>0){
-    mensagem += Serial.readStringUntil('\n');
-    mqtt.publish(OtherTopic.c_str(), mensagem.c_str());
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duracao = pulseIn(echoPin, HIGH);
+  distancia = duracao * 0.0343 / 2;
+
+  if (distancia > 0 && distancia < 10 && !detectou) {
+    detectou = true;
+    digitalWrite(ledPin, HIGH);
+    mqtt.publish(OtherTopic.c_str(), "Objeto detectado!");
+    Serial.println("Objeto detectado!");
+  } 
+  else if (distancia >= 10 && detectou) {
+    detectou = false;
+    digitalWrite(ledPin, LOW);
+    mqtt.publish(OtherTopic.c_str(), "Área livre");
+    Serial.println("Área livre");
   }
+
   mqtt.loop();
-  delay(1000);
+  delay(300);
 }
 
-void callback (char* topic, byte* payload, unsigned int lenght){
+void callback(char* topic, byte* payload, unsigned int length) {
   String mensagem = "";
-  for(int i = 0 ;i< lenght; i++){
+  for (int i = 0; i < length; i++) {
     mensagem += (char)payload[i];
-
   }
-  Serial.print("Recebido:");
+
+  Serial.println("Recebido:");
   Serial.println(mensagem);
+
+  if (mensagem.equalsIgnoreCase("acender")) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED ACESO!");
+  } 
+  else if (mensagem.equalsIgnoreCase("apagar")) {
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED APAGADO!");
+  } 
+  else {
+    Serial.println("Mensagem não reconhecida.");
+  }
 }
